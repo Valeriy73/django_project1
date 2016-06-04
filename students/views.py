8bc893dc971fcde9bae58c46755cb538be829f83
Valeriy Tur <turlera@ukr.net>
2016-06-04 23:10:29 +0300

    add dinamic data

--- students/views.py
+++ students/views.py
@@ -1,9 +1,29 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for Students
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Віталій',
+         'last_name': u'Подоба',
+         'ticket': 235,
+         'image': 'img/student1.jpg'},
+        {'id': 2,
+         'first_name': u'Андрій',
+         'last_name': u'Короста',
+         'ticket': 2123,
+         'image': 'img/student2.jpg'},
+        {'id': 3,
+         'first_name': u'Вареник',
+         'last_name': u'Петро',
+         'ticket': 5323,
+         'image': 'img/student3.jpeg'}
+         )
+    return render(request, 'students/students_list.html',
+                  {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Students ADD Form</h1>')

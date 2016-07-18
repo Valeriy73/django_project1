2ba2433a8d55869038c8fcc323e5022940e72737
Valeriy Tur <turlera@ukr.net>
2016-07-18 05:28:35 +0300

    change template journal

--- students/views/journal.py
+++ students/views/journal.py
@@ -6,12 +6,20 @@ from django.http import HttpResponse
 
 # Views for Journal
 def journal_list(request):
+    journal = (
+        {'id': 1,
+         'name': u'Подоба Віталій'},
+         {'id': 2,
+         'name': u'Корост Андрій'},
+         {'id': 3,
+         'name': u'Притула Тарас'}
+    )
     return render(request, 'students/journal.html',
-                  {})
+                  {'journal': journal})
 
-def journal_stud(request):
+def journal_stud(request, jid):
     return HttpResponse('<h1>Journal student %s</h1>' %jid)
 
-def journal_update(request, gid):
+def journal_update(request):
     return HttpResponse('<h1>Update journal</h1>')
 # Create your views here.
